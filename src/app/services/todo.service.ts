9d05b7aa43f749a5f8cac18f499bbb8bfa9b2800
Leah Deshar <leah.desar@gmail.com>
2024-05-05 14:49:38 +0545

    continue

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,13 +1,28 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
   constructor(private httpClient: HttpClient) {}
 
-  fetch() {
-    return this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
+  fetch(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>(this.apiUrl).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const errorMessage = `Error fetching todos: ${error.message}`;
+        return throwError(() => errorMessage);
+      })
+    );
   }
 }
