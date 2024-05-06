7d734258e5d1df5aa77c8b8ea08ed7deec4978b4
Leah Deshar <leah.desar@gmail.com>
2024-05-07 00:29:09 +0545

    user service

--- src/app/user/auth/auth.component.ts
+++ src/app/user/auth/auth.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  Username: string = '';
 
+  onInputChange(inputElement: HTMLInputElement) {
+    this.Username = inputElement.value;
+    console.log(this.Username);
+  }
 }
