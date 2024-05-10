2f5c232a0c6381d5b6cba76574dda46178bbf078
Leah Deshar <leah.desar@gmail.com>
2024-05-10 23:55:06 +0545

    reactive forms

--- src/app/user/auth/auth.component.ts
+++ src/app/user/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-class User {
+class Register {
   name: string = '';
   email: string = '';
   image: string = 'Profile Upload';
@@ -9,6 +9,12 @@ class User {
   address: string = '';
   password: string = '';
   confirmPass: string = '';
+}
+
+class Login {
+  email: string = '';
+
+  password: string = '';
 }
 @Component({
   selector: 'app-auth',
@@ -18,10 +24,16 @@ class User {
   styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-  user: User = new User();
-  signUp: boolean = false;
+  user: Register = new Register();
+  login: Login = new Login();
+
+  signUp: boolean = true;
 
   onSubmitRegister() {
+    console.log(this.user);
+  }
+  onSubmitLogin() {
+    console.log('Login');
     console.log(this.user);
   }
 }
