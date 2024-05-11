089ef6a030bdef33735ffbbdecc39ecdc2638866
Leah Deshar <leah.desar@gmail.com>
2024-05-12 00:26:17 +0545

    data binding and add services

--- src/app/user/userservice/authservice.service.ts
+++ src/app/user/userservice/authservice.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthserviceService {
+  constructor() {}
 
-  constructor() { }
+  // Check if the user is authenticated
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+  // Log the user out
+  logout() {
+    localStorage.removeItem('token');
+  }
+  // Save the token in the local storage
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  // login
+  login(data: any) {
+    localStorage.setItem('token', data.token);
+  }
+
+  // register
+  register(data: any) {
+    localStorage.setItem('token', data.token);
+  }
 }
